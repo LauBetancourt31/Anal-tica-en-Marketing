c2ae8ff3c2e893288dcef53b44c373a24764295b
LauBetancourt31 <laura.betancourt1@udea.edu.co>
2024-10-03 20:07:11 -0500

    Arreglos

--- funciones.py
+++ funciones.py
@@ -3,3 +3,10 @@ def ejecutar_sql (nombre_archivo, cur):
     sql_as_string=sql_file.read()
     sql_file.close
     cur.executescript(sql_as_string)
+    
+def ejecutar_sql2(nombre_archivo, cur):
+       sql_file = open(nombre_archivo)
+       sql_as_string = sql_file.read()
+       sql_file.close()
+       print(sql_as_string) # Add this line to print the SQL script
+       cur.executescript(sql_as_string)
